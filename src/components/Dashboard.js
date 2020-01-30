74850ac71b2108fe3f4055b9b4ae8da288d71575
Porson <porsonlee@gmail.com>
2020-01-29 19:57:29 -0800

    fetched appointment data by using promise

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Loading from "components/Loading";
 import Panel from "components/Panel";
 import classnames from "classnames";
+import axios from "axios";
+import { getTotalInterviews, getLeastPopularTimeSlot, getMostPopularDay, getInterviewsPerDay } from "helpers/selectors";
 
 
 // SUPER FAKE DATA
@@ -10,22 +12,22 @@ const data = [
   {
     id: 1, 
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -36,8 +38,11 @@ class Dashboard extends Component {
 
   //initial state
   state = {
-    loading: false,
-    focused: null
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {},
   } 
 
   componentDidMount() {
@@ -46,6 +51,20 @@ class Dashboard extends Component {
     if(focused) {
       this.setState({ focused });
     }
+   
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.dataa
+      });
+    })
+
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -62,6 +81,7 @@ class Dashboard extends Component {
 
   // render
   render() {
+    // console.log(this.state)
     const dashboardClasses = classnames("dashboard", {"dashboard--focused": this.state.focused});
     
 
@@ -78,8 +98,8 @@ class Dashboard extends Component {
         key={panel.id}
         // id={panel.id}
         label={panel.label}
-        value={panel.value}
-        onSelect={event => this.selectPanel(panel.id)}
+        value={panel.getValue(this.state)}
+        onSelect={() => this.selectPanel(panel.id)}
       />
     ));
 
